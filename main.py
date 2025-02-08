2bb7c1e08d28973ba28a9161c06d9de835511c9e
psxks <shizenking@gmail.com>
2025-02-08 13:49:56 +0200

    core loop

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class GameCreator():
         
         self.functions = []
         
-        self.core_loop = []
+        self.core_loop = [[],[],[]]
                 
         self.mechanics = False
         self.screen = False
@@ -54,7 +54,7 @@ class GameCreator():
         main.write('\n')
             
         if self.game:
-            main.write('class Game():\n    def __init__(self):\n        pygame.init()')
+            main.write('class Game():\n    def __init__(self):\n        pygame.init()\n')
             
         for i in self.values:
             if isinstance(i, list):
@@ -65,26 +65,26 @@ class GameCreator():
         
         main.write('\n')
         
-        main.write(f"pygame.display.set_caption('{self.name}')")
+        main.write(f"pygame.display.set_caption('{self.name}')\n")
         
         main.write('\n')
         
         if len(self.anim) > 0:
-            main.write("       self.animations = {")
+            main.write("       self.animations = {\n")
             for i in self.anim:
-                main.write(f'           {i[0]}: Animation({i[1]}, img_dur={i[2]}, loop={i[3]})')
-            main.write("       }")
+                main.write(f'           {i[0]}: Animation({i[1]}, img_dur={i[2]}, loop={i[3]})\n')
+            main.write("       }\n")
         
         buttons = []
         
         main.write('\n')
         
         if len(self.ui) > 0:
-            main.write("       self.ui = {")
+            main.write("       self.ui = {\n")
             for i in self.ui:
                 buttons.append(i[0])
-                main.write(f"           '{i[0]}': SkillsUI({i[1]},{i[2]}, load_image('{i[3]}'), {i[4]}, {i[5]}, {i[6]} , '{i[7]}')")
-            main.write("       }")        
+                main.write(f"           '{i[0]}': SkillsUI({i[1]},{i[2]}, load_image('{i[3]}'), {i[4]}, {i[5]}, {i[6]} , '{i[7]}')\n")
+            main.write("       }\n")        
         
         main.write('\n')
         
@@ -95,10 +95,12 @@ class GameCreator():
         for func in self.functions:
             main.write(func)
         
-        main.write('\n\n    def run(self):\n      while True:')
+        main.write('\n\n    def run(self):\n        while True:')
         
-        for i in 
-        
+        for i in self.core_loop:
+            for j in i:
+                for l in j:
+                    main.write('            '+j+'\n')
         
         if self.screen:
             vert_shader = open(f"{self.game_path}/shaders/shader.vert", "w")
@@ -106,11 +108,6 @@ class GameCreator():
             
             frag_shader = open(f"{self.game_path}/shaders/shader.frag", "w")
             frag_shader.write(shaders['frag'])
-        
-        for i in self.values:
-            for j in i:
-                
-        
         
         if self.game:
             main.write('if __name__ == "__main__":\n    Game().run()')
@@ -131,8 +128,6 @@ class GameCreator():
             6(level_name, path) - add tileset; tileset script;
             7(name, width, height, img, x, y, kd, key) - add ui; ui script;
             8 - add player; player script;
-            9 - add fps;
-            10 - add buff script;
             ''')
         
     def run(self):
@@ -154,6 +149,17 @@ class GameCreator():
                     
                     self.values.append(["self.clock = pygame.time.Clock()", "self.t = 0"])
                     
+                    self.core_loop[0].append([
+                        "           self.t += self.clock.get_time() / 1000",
+                    ])
+                    
+                    self.core_loop[1].append([
+                        "           for event in pygame.event.get():",
+                        "               if event.type == pygame.QUIT:",
+                        "                   pygame.quit()",
+                        "                   sys.exit()",
+                    ])
+
                     print('Successfully added class added')
                 
                 elif '2' in us_write:
@@ -180,6 +186,43 @@ class GameCreator():
                         "self.target_zoom = 1.0",
                         "self.screen_shake = 0 "
                                         ])
+
+                    self.core_loop[0].append([
+                        "self.target_zoom = 1.0",
+                        "self.current_zoom += (self.target_zoom - self.current_zoom) * 0.1\n",
+                        "self.ui_surf.fill((0,0,255))",
+                        "self.main_surf.fill((198, 183, 190))",
+                        "self.decoration_surf.fill((0, 0, 0, 0))\n",
+                        "self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30",
+                        "self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30",
+                        "render_scroll = (int(self.scroll[0]), int(self.scroll[1]))\n",
+                        "if self.screen_shake > 0:",
+                        "   self.screen_shake -= 1",
+                        "   self.scroll[0] += random.randint((self.screen_shake//5) * -1, (self.screen_shake//5))",
+                        "   self.scroll[1] += random.randint((self.screen_shake//5) * -1, (self.screen_shake//5))\n",
+                        "display_mask = pygame.mask.from_surface(self.display)",
+                        "display_sillhouette = display_mask.to_surface(setcolor=(0, 0, 0, 180), unsetcolor=(0, 0, 0, 0))\n",
+                        "for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:",
+                        "   self.display.blit(display_sillhouette, offset)\n",
+                    ])
+                    
+                    self.core_loop[2].append([
+                        "zoomed_size = (int(self.main_surf.get_width() * self.current_zoom), int(self.main_surf.get_height() * self.current_zoom))",
+                        "offset_x = (self.main_surf.get_width() - zoomed_size[0]) // 2",
+                        "offset_y = (self.main_surf.get_height() - zoomed_size[1]) // 2\n",
+                        "zoomed_main_surf = pygame.transform.smoothscale(self.main_surf, zoomed_size)",
+                        "self.display.blit(zoomed_main_surf, (offset_x, offset_y))\n",
+                        "self.decoration_surf.set_colorkey((0, 0, 0))",
+                        "zoomed_decoration_surf = pygame.transform.smoothscale(self.decoration_surf, zoomed_size)",
+                        "self.display.blit(zoomed_decoration_surf, (offset_x, offset_y))\n",
+                        "self.ui_surf.set_colorkey((0,0,255))\n",
+                        "screen_surface = pygame.transform.scale(self.display, self.screen.get_size())",
+                        "screen_surface.blit(self.ui_surf, (0,0))\n",
+                        "self.main_shader.render(screen_surface, self.t)\n",
+                        "pygame.display.flip()",
+                        "self.clock.tick(60)"
+                        
+                    ])
                     
                     print('Successfully added screen')
                     
@@ -195,6 +238,13 @@ class GameCreator():
                         'from scripts.particles import Particle, load_particle_images'
                     )
                     
+                    self.core_loop[0].append([
+                        "for particle in self.particles[:]:",
+                        "   particle.update(self.clock.get_time() / 45)\n",
+                        "for particle in self.particles:",
+                        "   particle.draw(self.main_surf, self.scroll)\n",
+                    ])
+                    
                     self.values.append(f"load_particle_images('f{self.get_input_values(us_write)[0]}')")
                     
                     print('Successfully particles added')
@@ -207,6 +257,15 @@ class GameCreator():
                         "self.load_level(self.level)"
                                         ])
                     
+                    self.core_loop[0].append([
+                        "self.tilemap.render(",
+                        "   self.main_surf,",
+                        "   self.decoration_surf,",
+                        "   self.tileset,",
+                        "   offset=render_scroll,",
+                        ")\n"
+                    ])
+                    
                     self.functions.append(cycle_parts['load_lvl'])
                     
                     print('Successfully tileset added')
@@ -218,6 +277,11 @@ class GameCreator():
                     
                     self.ui.append(self.get_input_values(us_write))
                     
+                    self.core_loop[2].append([
+                        "for name, obj in self.ui.items():",
+                        "   obj.render(self.ui_surf, state)\n"
+                    ])
+                    
                     print('Successfully ui added')
                 
                 elif us_write == '8':
@@ -227,17 +291,12 @@ class GameCreator():
                         "self.player = Player(self, (50, 50), (8, 15))",
                     ])
                     
-                    print('Successfully player added')
-                
-                elif us_write == '9':
-                    print('Successfully fps added')
-                
-                elif us_write == '10':
-                    self.imports.append(
-                        'from scripts.buff import *'
-                    )
+                    self.core_loop[0].append([
+                        "self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))",
+                        "self.player.render(self.main_surf, offset=render_scroll)\n"
+                    ])
                     
-                    print('Successfully buffs added')
+                    print('Successfully player added')
                     
                 else:
                     print('error')
